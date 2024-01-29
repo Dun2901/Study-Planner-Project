f3c0adf9fb84c6476b851d73c0fba80b87c01dbd
Huy Chi Nguyen <nguye2cy@mail.uc.edu>
2024-01-29 14:07:17 -0500

    Create table and dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="m-12">hello</div>;
+  return (
+  <div className="App">
+    <div className="m-12">
+      <p>hello</p>
+          <table>
+            <tr>
+              <th>Nhiệm Vụ</th>
+              <th>Người được giao việc</th>
+              <th>Deadline</th>
+              <th>Trạng thái</th>
+            </tr>
+            <tr>
+              <td>New 1</td>
+              <td>Leo</td>
+              <td>12-1</td>
+              <td>In Progress</td>
+            </tr>
+          </table>
+
+    </div>
+  </div>
+  );
 }
-//hello Dun
+
 export default App;
