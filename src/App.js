10e66ad80fd681e87cca6f8f98c6a8c8780e0250
Huy Chi Nguyen <nguye2cy@mail.uc.edu>
2024-01-30 11:55:32 -0500

    Create folders and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import StudyGoal from "./components/Study Goals/study-goals";
 import DevPlan from "./components/Development Plan/dev-plan";
 
 function App() {
   return (
   <div className="App">
     <div className="m-12">hello</div>
+    <StudyGoal />
     <DevPlan />
   </div>
   );
---
+++ src/components/Study Goals/study-goals.js
@@ -0,0 +1,9 @@
+import "./study-goals.css"
+
+function StudyGoal() {
+    return (
+        <div>nice</div>
+    );
+}
+
+export default StudyGoal;
