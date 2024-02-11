d6f60a652f3b6a35f54d5ea6300fe9b223632e84
Huy Chi Nguyen <nguye2cy@mail.uc.edu>
2024-02-11 14:46:23 -0500

    Finish up Development Plan section and fix some stuff

--- src/components/Development Plan/dev-plan.js
+++ src/components/Development Plan/dev-plan.js
@@ -3,24 +3,39 @@ import "./dev-plan.css";
 function DevPlan() {
     return (
         <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Nhiệm Vụ</th>
-                <th>Người được giao việc</th>
-                <th>Deadline</th>
-                <th>Trạng thái</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>New 1</td>
-                <td>Leo</td>
-                <td>12-1</td>
-                <td>In Progress</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="title">Development Plan</div>
+          <body>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nhiệm Vụ</th>
+                  <th>Người được giao việc</th>
+                  <th>Deadline</th>
+                  <th>Trạng thái</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>New 1</td>
+                  <td>Leo</td>
+                  <td>12-1</td>
+                  <td>In Progress</td>
+                </tr>
+                <tr>
+                  <td>New 2</td>
+                  <td>Dun</td>
+                  <td>12-5</td>
+                  <td>Todo</td>
+                </tr>
+                <tr>
+                  <td>New 3</td>
+                  <td>DT</td>
+                  <td>12-8</td>
+                  <td>Done</td>
+                </tr>
+              </tbody>
+            </table>
+          </body>
       </div>
     );
 }
